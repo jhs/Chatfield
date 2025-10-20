3fba6ed811ace3fc3c6ff4533de3b9e5feb0ce25
Jason Smith (FS) <jason.h.smith@gmail.com>
2025-10-20 10:20:31 -0500

    Run: mix phx.gen.live Blog Post posts title:string body:text

---
+++ TypeScript/examples/cp/test/support/fixtures/blog_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule Cp.BlogFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Cp.Blog` context.
+  """
+
+  @doc """
+  Generate a post.
+  """
+  def post_fixture(attrs \\ %{}) do
+    {:ok, post} =
+      attrs
+      |> Enum.into(%{
+        body: "some body",
+        title: "some title"
+      })
+      |> Cp.Blog.create_post()
+
+    post
+  end
+end
